09ef7b919caed7b3e712dcdf46b69343300b6998
Alan Yan <alanyan@princeton.edu>
2023-04-02 16:30:11 -0400

    hey

--- code/check-conjectures.py
+++ code/check-conjectures.py
@@ -8,50 +8,56 @@ def powerset(iterable):
 
 class matroid:
     # defines the matroid
-    # id - identification of the matroid (int)
-    # size - size of the matroid (int)
-    # rank - rank of the  matroid (int)
-    # bases - list of bases (array of strings)
+    # variables: id, size, rank, bases
+    # id - integer
+    # size - integer
+    # rank - integer
+    # bases - array of strings
     def __init__(self, id, size, rank, bases):
         self.id = int(id)
         self.size = int(size)
         self.rank = int(rank)
         self.bases = bases
-    # returns true if simple; otherwise, return false
-    def simple(self):
-        def check_i(i): # checks if i is in at least one base
-            for base in self.bases:
-                if str(i) in base:
-                    return True
-            return False
-        def check_pair(i, j): # checks if i, j are both in at least one base together
-            for base in self.bases:
-                if str(i) in base and str(j) in base:
-                    return True
-            return False
-        # check for loops
-        for i in range(self.size):
-            if not check_i(i):
-                return False
-        # checks for parallel elements
-        for i in range(self.size):
-            for j in range(i):
-                if not check_pair(i, j):
-                    return False
-        return True
-    
-    def intersection(self, A, base):
-        num = 0
-        for a in A:
-            if str(a) in base:
-                num += 1
-        return num
     
     def coloop(self, e):
         for base in self.bases:
             if str(e) not in base:
                 return False
         return True
+
+    def loop(self, e):
+        for base in self.bases:
+            if str(e) in base:
+                return False
+        return True
+    
+    def parallel(self, i, j):
+        if i == j:
+            print("Error Parallel")
+        for base in self.bases:
+            if str(i) in base and str(j) in base:
+                return False
+        return True
+
+    # Checks if matroid is simple
+    def simple(self):
+        # check for loops
+        for i in range(self.size):
+            if self.loop(i):
+                return False
+        # checks for parallel
+        for i in range(self.size):
+            for j in range(i):
+                if self.parallel(i, j):
+                    return False
+        return True
+    """
+    def intersection(self, A, base):
+        num = 0
+        for a in A:
+            if str(a) in base:
+                num += 1
+        return num
     def show_yourself(self, A, point):
         return "ID : " + str(self.id) + " ... SIZE :" + str(self.size) + " ... BASE : ... " + str(self.bases) + " ... at " + str(point) + " and partition " + str(A)
 
@@ -64,7 +70,6 @@ class matroid:
                 if sequence[k]**2 * math.comb(self.rank, k+1) * math.comb(self.rank, k-1) == sequence[k-1] * sequence[k+1] * math.comb(self.rank, k)**2:
                     self.show_yourself(A, k)
                     return True # this indicates that there is a counterexample
-
     def check_conjecture(self):
         if self.id % 10 == 0:
             print("checking " + str(self.id))
@@ -74,34 +79,40 @@ class matroid:
                 for k in range(self.rank):
                     self.check_conjecture_specific(A)
         return False
+    """
 
+    # checks the conjecture for HRR_1
+    # returns True if the conjecture is False
     def check_HRR_conjecture(self):
+        if self.id % 100 == 0:
+            print(self.id)
         if self.simple():
             for i in range(self.size):
-                if self.HRR(i):
+                if self.HRR_Helper(i):
                     print(str(self.id) + " and at e = " + str(i))
                     return True
         return False
     
-    def HRR(self, m):
-        n = self.size
-        if not self.coloop(m):
-            hessian = np.zeros((n, n))
-            for i in range(n):
-                for j in range(i):
-                    if i == m or j == m:
-                        for base in self.bases:
-                            if str(i) in base and str(j) in base:
-                                hessian[i][j] += 1
-                                hessian[j][i] += 1
-                    else:
-                        for base in self.bases:
-                            if str(i) in base and str(j) in base and str(m) not in base:
-                                hessian[i][j] += 1
-                                hessian[j][i] += 1
-            if np.linalg.det(hessian) == 0:
-                return True # this indicates that there is a counterexample
-        return False
+    # checks the conjecture for HRR_1 at e = m
+    # returns True if the conjecture is false at e = m
+    def HRR_Helper(self, m):
+        if self.coloop(m):
+            return False
+
+        Hess = np.zeros((self.size, self.size)) # Hessian of Basis Generating Polynomial
+        for i in range(self.size):
+            for j in range(i):
+                if i == m or j == m:
+                    for base in self.bases:
+                        if str(i) in base and str(j) in base:
+                            Hess[i][j] += 1
+                            Hess[j][i] += 1
+                else:
+                    for base in self.bases:
+                        if str(i) in base and str(j) in base and str(m) not in base:
+                            Hess[i][j] += 1
+                            Hess[j][i] += 1
+        return np.linalg.det(Hess) == 0
 
 
 def convert_to_matroid(line):
@@ -117,9 +128,5 @@ file = open("C:/senior-thesis-data/matroids09_bases",  'r')
 lines = file.readlines()
 for l in lines:
     mat = convert_to_matroid(l)
-    print(mat.id)
     if mat.check_HRR_conjecture():
         break
-
-
-# The result of the program: none of the matroids in the list break the conjecture!
